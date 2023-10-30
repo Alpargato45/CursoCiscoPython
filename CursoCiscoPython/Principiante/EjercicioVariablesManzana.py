32c50abfa0822f76dc2672c66e7bbcb7e75a2ed9
Jorge del Cid <jorgedelcid2004@gmail.com>
2023-10-30 19:43:44 +0100

    Primer Commit

---
+++ CursoCiscoPython/Principiante/EjercicioVariablesManzana.py
@@ -0,0 +1,7 @@
+juan = 3
+maria = 5
+adan = 6
+
+totalManzanas = juan + maria + adan
+print("Juan tiene", juan, "manzanas\n" "María tiene", maria, "manzanas\n" "Adán tiene", adan, "manzanas")
+print("En total tienen", totalManzanas, "manzanas")
