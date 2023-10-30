0d3bc05959187b05897acabe2797fd86bd015b92
Jorge del Cid <jorgedelcid2004@gmail.com>
2023-10-30 20:23:07 +0100

    Tercer Commit

---
+++ CursoCiscoPython/Principiante/MuestraValores.py
@@ -0,0 +1,9 @@
+valor1 = float(input("Elige el primer float"))
+valor2 = float(input("Elige el segundo float"))
+
+print(valor1 + valor2)
+print(valor1 - valor2)
+print(valor1 * valor2)
+print(valor1 / valor2)
+
+print("\n¡Eso es todo, amigos!")
