3195ea956fb2bea22282ed32c6670754f7cabd9e
Alpargato45 <jorgedelcid2004@gmail.com>
2023-12-15 12:11:33 +0100

    Ejercicios

---
+++ CursoCiscoPython/Tema 3/quitarRepetidosArray.py
@@ -0,0 +1,10 @@
+﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+
+for i in my_list:
+    aux = my_list[i]
+    for j in range (1,len(my_list)):
+        if(i == j):
+            del my_list[j]
+
+print("La lista con elementos únicos:")
+print(my_list)
---
+++ CursoCiscoPython/Tema 3/quitarRepetidosConIn.py
@@ -0,0 +1,8 @@
+﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+
+for i in my_list:
+    if i in my_list:
+        del my_list[i]
+
+print("La lista con elementos únicos:")
+print(my_list)
