5f28da41a10155a76cc2113d4d64f047b3d56492
Alpargato45 <jorgedelcid2004@gmail.com>
2023-12-15 10:08:45 +0100

    Ejercicios

---
+++ CursoCiscoPython/Principiante/quitarVocales.py
@@ -0,0 +1,13 @@
+﻿# Indicar al usuario que ingrese una palabra
+# y asignarlo a la variable user_word.
+
+print("Escribe tu palabra")
+user_word = input()
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == "A" or letter == "E" or letter == "I" or letter == "O" or letter == "U":
+        continue
+    print(letter)
+    
+
