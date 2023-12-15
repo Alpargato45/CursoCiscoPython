955714186a081fce30763859b6ec7738ddca4bf2
Alpargato45 <jorgedelcid2004@gmail.com>
2023-12-15 10:51:20 +0100

    Ejercicios

---
+++ CursoCiscoPython/Tema 3/lotharCollatz.py
@@ -0,0 +1,17 @@
+﻿print("Di tu numero")
+c0 = int(input())
+pasos = 0
+
+while c0 <= 0:
+    print("Di tu numero")
+    c0 = int(input())
+while c0 != 1:
+    if c0 % 2 == 0:
+        c0 = c0//2
+        pasos = pasos+1
+        print(c0)
+    else:
+        c0 = 3 * c0 + 1
+        pasos = pasos+1
+        print(c0)
+print("pasos = ", pasos)
